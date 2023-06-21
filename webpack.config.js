d315a183ddee445b4cb89fe78a80b2b2ca9e05b5
Adrian Edwards <adrian@adriancedwards.com>
2023-06-21 12:12:53 -0700

    lint fixes to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 // you can just require .json, saves the 'fs'-hassle
 const path = require('path');
 
-const pkgjson = require('./package.json');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
+const pkgjson = require('./package.json');
 
 const BUILD_DIR = path.resolve(__dirname, 'build');
 const DIST_DIR = path.resolve(__dirname, 'dist');
@@ -12,24 +12,21 @@ const MANIFEST_FILE = 'manifest.json';
 
 const manifestPath = path.join(SRC_DIR, MANIFEST_FILE);
 
-
 function transformName(input) {
+	let names = input.split('-');
 
-	let names = input.split("-");
-
-	names = names.map((val, index) => {
-		if (val == "rit") {
-			return val.toUpperCase()
-		} else {
-			return val.charAt(0).toUpperCase() + val.slice(1);
+	names = names.map((val) => {
+		if (val === 'rit') {
+			return val.toUpperCase();
 		}
-	})
-	return names.join(" ")
+		return val.charAt(0).toUpperCase() + val.slice(1);
+	});
+	return names.join(' ');
 }
 
 function modify(buffer) {
 	// copy-webpack-plugin passes a buffer
-	var manifest = JSON.parse(buffer.toString());
+	const manifest = JSON.parse(buffer.toString());
 
 	// make any modifications you like, such as
 	manifest.version = pkgjson.version;
@@ -38,14 +35,13 @@ function modify(buffer) {
 	manifest.name = transformName(pkgjson.name);
 
 	// pretty print to JSON with two spaces
-	manifest_JSON = JSON.stringify(manifest, null, 2);
-	return manifest_JSON;
+	return JSON.stringify(manifest, null, 2);
 }
 
-
 module.exports = {
-	//For some reason, webpack insists on having an entrypoint and making some JS
-	//here we give it an entrypoint it cant make anything useful from and then later we use FileManagerPlugin to delete the generated file
+	// For some reason, webpack insists on having an entrypoint and making some JS
+	// here we give it an entrypoint it cant make anything useful from and then later we use
+	// FileManagerPlugin to delete the generated file
 	// webpack is only being used to copy data from package.json into manifest.json
 	entry: ['./src/manifest.json'],
 	plugins: [
@@ -54,11 +50,11 @@ module.exports = {
 				{
 					from: manifestPath,
 					to: BUILD_DIR,
-					transform(content, path) {
-						return modify(content)
-					}
-				}
-			]
+					transform(content) {
+						return modify(content);
+					},
+				},
+			],
 		}),
 		new FileManagerPlugin({
 			events: {
@@ -66,7 +62,7 @@ module.exports = {
 					delete: [path.join(DIST_DIR, 'main.js')],
 				},
 			},
-		})
-	]
+		}),
+	],
 
 };
