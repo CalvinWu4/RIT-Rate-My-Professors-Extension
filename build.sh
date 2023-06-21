ed2c61853c21f72ab2ee5127471e4f07b6405fb3
Adrian Edwards <adrian@adriancedwards.com>
2023-06-21 12:04:47 -0700

    build script edits

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-mkdir -p build; rm -rf build/*
-mkdir -p dist; rm -rf dist/*
+mkdir -p build
+mkdir -p dist
 cp -r src build/firefox
 cp -r src build/chrome
-node mixin.js firefox/manifest.json build/firefox/manifest.json
+cp -r build/manifest.json build/chrome
+node mixin.js firefox/manifest.json build/manifest.json
+
+cp -r build/manifest.json build/firefox
+
 cd build/chrome; zip -r ../../dist/chrome.zip *; cd ../..
 cd build/firefox; zip -r ../../dist/firefox.xpi *; cd ../..
 
