d975d4706cf9cced2a1c9bcca511033bba54938f
Adrian Edwards <adrian@adriancedwards.com>
2023-06-21 18:06:38 -0700

    start populating RMPData class

--- src/content/rmpdata.js
+++ src/content/rmpdata.js
@@ -1,4 +1,20 @@
 export default class RMPData {
 
+	constructor(firstName, year) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.department = department;
+		this.qualityRating
+		this.ratingsCount
+		this.difficultyGPA
+		this.wouldTakeAgainPercentage
+		this.topTags = []
+	}
+
+
+	fromGraphQL(data) {
+
+	}
 	
 }
