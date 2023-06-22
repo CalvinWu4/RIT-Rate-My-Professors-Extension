fe02e3073ce8b4a225fffe78f809eaa9d1066a6e
Adrian Edwards <adrian@adriancedwards.com>
2023-06-21 18:13:43 -0700

    finish constructor for RMPProfessorData

--- src/content/rmpprofessordata.js
+++ src/content/rmpprofessordata.js
@@ -1,15 +1,16 @@
 export default class RMPProfessorData {
 
-	constructor(firstName, year) {
-		this.firstName = firstName;
-		this.middleName = middleName;
-		this.lastName = lastName;
-		this.department = department;
-		this.qualityRating
-		this.ratingsCount
-		this.difficultyGPA
-		this.wouldTakeAgainPercentage
-		this.topTags = []
+	constructor(firstName, middleName, lastName, department, qualityRating, ratingsCount, difficultyGPA, wouldTakeAgainPercentage, topTags, mostHelpfulRating) {
+		this.firstName = firstName || "";
+		this.middleName = middleName || "";
+		this.lastName = lastName || "";
+		this.department = department || "";
+		this.qualityRating = qualityRating || 0;
+		this.ratingsCount = ratingsCount || 0;
+		this.difficultyGPA = difficultyGPA || 0.0;
+		this.wouldTakeAgainPercentage = wouldTakeAgainPercentage || 0;
+		this.topTags = topTags || [];
+		this.mostHelpfulRating = mostHelpfulRating;
 	}
 
 
